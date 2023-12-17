c3cf347fb3c4e439ac77540e0731de4247fa0da1
Chiara Lunghi <127502273+Chia2500@users.noreply.github.com>
2023-12-17 11:53:27 +0100

    piccole modifiche

--- test/real_time.py
+++ test/real_time.py
@@ -1,28 +1,30 @@
-
 from pydub import AudioSegment
 from pygame import mixer
 import threading
 import time
 import tempfile
+import os
 from music_player import Play_Pause
 from BPM_extractor import get_bpm 
 
-# Variabile globale per memorizzare il BPM precedente
-previous_input_bpm = get_bpm('percorso/della/traccia/audio.mp3')
+song_path = 'percorso/della/traccia/audio.mp3'
+original_bpm = get_bpm(song_path)
+window_size_ms = 5000  # Larghezza di finestra in millisecondi
 
-def update_bpm(new_input_bpm, previous_input_bpm):
+def obtain_next_buffer():
     # Carica la traccia audio
-    audio = AudioSegment.from_file('percorso/della/traccia/audio.mp3')
+    audio = AudioSegment.from_file(song_path, format="mp3")
+    return next_buffer
 
-    # Calcola il rapporto di velocità necessario
+def update_bpm(new_input_bpm, previous_input_bpm, next_buffer):
     speed_ratio = new_input_bpm / previous_input_bpm
 
     # Modifica la velocità senza alterare la tonalità
-    modified_audio = audio.speedup(playback_speed=speed_ratio)
+    modified_buffer = audio.speedup(playback_speed=speed_ratio)
 
     # Salva il brano modificato in un file temporaneo
     _, temp_path = tempfile.mkstemp(suffix=".mp3")
-    modified_audio.export(temp_path, format="mp3")
+    modified_buffer.export(temp_path, format="mp3")
 
     # Riproduci la traccia modificata
     mixer.music.stop()
@@ -33,15 +35,38 @@ def update_bpm(new_input_bpm, previous_input_bpm):
     mixer.music.queue(lambda: os.remove(temp_path))
 
 
-def update_bpm_continuously(new_input_bpm):
-    global previous_input_bpm
-    while True:
+def extract_and_modify_buffers(song_path, window_size_ms, new_input_bpm_bpm):
+    # Carica la canzone
+    song = AudioSegment.from_file(song_path)
 
+    # Calcola il numero di frame in base alla larghezza di finestra
+    frame_size = int(window_size_ms * song.frame_rate / 1000)
+
+    # Estrai i buffer con la larghezza di finestra specificata
+    buffers = [song[i:i+frame_size] for i in range(0, len(song), frame_size)]
+
+    # Modifica la velocità di ciascun buffer in base ai BPM desiderati
+    modified_buffers = [
+        buffer.speedup(playback_speed=new_input_bpm / (len(buffer) / 1000 * 60))
+        for buffer in buffers
+    ]
+
+    return modified_buffers
+
+
+# Salva i buffer modificati in file audio separati
+# for i, buffer in enumerate(modified_buffers):
+#     buffer.export(f"percorso/buffer_modificato_{i + 1}.wav", format="wav")
+
+
+
+def update_bpm_continuously(new_input_bpm):
+    previous_input_bpm = original_bpm
+    while True:
         # Aggiorna il BPM solo se è diverso dal precedente input
         if new_input_bpm != previous_input_bpm:
             update_bpm(new_input_bpm, previous_input_bpm)
             previous_input_bpm = new_input_bpm
-
         time.sleep(1)
 
 
