81d227e310f0b48d44a3859d30621b316aacc27c
marcellograti <marcello.grati@mail.polimi.it>
2024-05-17 15:13:13 +0200

    add SensorService module to the project

--- BuddyBeatApp/BuddyBeat/app/src/main/java/com/example/buddybeat/MainActivity.kt
+++ BuddyBeatApp/BuddyBeat/app/src/main/java/com/example/buddybeat/MainActivity.kt
@@ -28,11 +28,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
@@ -120,6 +123,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startService(Intent(this, SensorService::class.java))
 
         setContent {
             BuddyBeatTheme {
---
+++ BuddyBeatApp/BuddyBeat/app/src/main/java/com/example/buddybeat/SensorService.kt
@@ -0,0 +1,135 @@
+package com.example.buddybeat
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.annotation.OptIn
+import androidx.media3.common.util.Log
+import androidx.media3.common.util.UnstableApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlinx.coroutines.cancel
+import kotlin.math.sqrt
+import java.util.LinkedList
+
+class SensorService : Service(), SensorEventListener {
+    // Override callback methods here
+
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private lateinit var sensorManager: SensorManager
+    private var gyroSensor: Sensor? = null
+    private var accelSensor: Sensor? = null
+
+    // Intervallo di tempo tra le letture dei sensori in millisecondi
+
+    private var lastUpdate: Long = 0
+    private var lastAcceleration: Float = 0f
+    private var acceleration: Float = 0f
+    private var currentAcceleration: Float = SensorManager.GRAVITY_EARTH
+    private var steps: Int = 0
+    private var lastStepTime: Long = 0
+    private var stepFrequency: Float = 0f
+
+    private val stepTimes = LinkedList<Long>()
+    private var startTime: Long = 0
+
+    private var gyroData: String = ""
+    private var accelData: String = ""
+    private var stepData: String = ""
+    private var stepFreq: String = ""
+
+    @OptIn(UnstableApi::class) override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        scope.launch {
+            while (true) {
+                Log.d("SensorService", "Step Count: $steps")
+                Log.d("SensorService", "Step Cadence: $stepFrequency")
+                delay(1000)
+            }
+        }
+        return START_STICKY
+    }
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+        val intent = Intent(this, SensorService::class.java)
+        startService(intent)
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onDestroy() {
+
+        scope.cancel()
+        sensorManager.unregisterListener(this, gyroSensor)
+        sensorManager.unregisterListener(this, accelSensor)
+        super.onDestroy()
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            if (it.sensor == gyroSensor) {
+                gyroData = "Gyroscope data: ${it.values[0]}, ${it.values[1]}, ${it.values[2]}"
+            } else if (it.sensor == accelSensor) {
+                val x = it.values[0]
+                val y = it.values[1]
+                val z = it.values[2]
+                accelData = "Accelerometer data: ${it.values[0]}, ${it.values[1]}, ${it.values[2]}"
+
+                lastAcceleration = currentAcceleration
+                currentAcceleration = sqrt((x * x + y * y + z * z))
+                val delta = currentAcceleration - lastAcceleration
+
+                /* filtro passa basso rispetto a delta */
+                acceleration = acceleration * 0.9f + delta
+                val threshold = 5 /* aggiustare il valore */
+
+                if (acceleration > threshold) {
+                    val now = System.currentTimeMillis()
+                    if ((now - lastUpdate) > 500) { // 0.5s minimum between each step
+                        lastUpdate = now
+                        steps++
+                        stepTimes.add(now)
+
+                        stepData ="Steps: $steps"
+
+                        // Remove times older than one minute
+                        while ((stepTimes.firstOrNull() ?: Long.MAX_VALUE) < now - 60000) {
+                            stepTimes.removeFirstOrNull()
+                        }
+
+                        // Calculate step frequency
+                        if (now - startTime < 60000) {  //60000 millesimi di secondo=1 min
+                            stepFrequency = steps.toFloat() / ((now - startTime) / 1000f) * 60f
+                        } else {
+                            stepFrequency = stepTimes.size.toFloat() / 60f
+                        }
+
+                        lastStepTime = now
+                        stepFreq = "Frequency: $stepFrequency step/min"
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+}
