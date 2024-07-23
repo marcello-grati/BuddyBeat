7b4975409c3963ec64fdac7861963df4bfac6a38
silviapasin <silvia.pasin@mail.polimi.it>
2024-07-23 17:22:10 +0200

    add bpm to AudioItem

--- BuddyBeatApp/BuddyBeat/app/src/main/java/com/example/buddybeat/ui/components/AudioItem.kt
+++ BuddyBeatApp/BuddyBeat/app/src/main/java/com/example/buddybeat/ui/components/AudioItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.buddybeat.data.models.Song
@@ -58,6 +59,11 @@ fun AudioItem(
                     maxLines = 1
                 )
             }
+            Text(
+                text = audio.bpm.toString(),
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.size(12.dp))
             Text(
                 text = timeStampToDuration(position = audio.duration.toLong())
             )
